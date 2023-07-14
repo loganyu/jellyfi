9cddd0e2cdc4891595507b054a3dc1dffc3e7234
Logan Yu <yu.logan@gmail.com>
2023-07-13 19:14:04 -0700

    add collections style

--- app/api/v1/collections/route.ts
+++ app/api/v1/collections/route.ts
@@ -3,7 +3,7 @@ import mockResponse from "./mock_response"
 
 export async function GET(request: NextRequest) {
     return NextResponse.json(mockResponse);
-    
+
     const collectionIds = request.nextUrl.searchParams.get("collectionIds")?.split(',') as string[];
 
     const responses = await Promise.all(collectionIds.map(collection_id => fetch('https://api.lenderlabs.xyz/api/get_collection?' + new URLSearchParams(
@@ -11,10 +11,12 @@ export async function GET(request: NextRequest) {
     ))));
     const data = await Promise.all(responses.map(response => response.json()));
     const collections = data.map(d => d.collection)
-    collections.forEach(c => {
+    collections.forEach((c, i) => {
         delete c.loans
         delete c.offers
+        c.id = collectionIds[i]
     })
+    console.log('collections', collections)
 
     return NextResponse.json(collections);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,20 @@
 import { useEffect, useState } from 'react';
 
 import Header from "./components/header";
+import Footer from "./components/footer";
 import { SidebarProvider } from "./context/SidebarContext";
 
 export default function Index(): JSX.Element {
   return (
     <SidebarProvider>
-      <Header />
-      <div className="flex dark:bg-gray-900">
-        <main className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]">
-          <HomePage />
-        </main>
+      <div className="flex flex-col h-screen dark:bg-black">
+        <Header />
+        <div className="flex flex-grow">
+          <main className="flex flex-grow order-2 flex-[1_0_16rem]">
+            <HomePage />
+          </main>
+        </div>
+        <Footer />
       </div>
     </SidebarProvider>
   );}
@@ -57,18 +61,57 @@ function HomePage(): JSX.Element {
     }
   }, [summaries])
   
-  console.log('collections', collections)
+  console.log('summaries', summaries);
+  console.log('collections', collections);
+  const portfolioInfo: any = {}
+  summaries.loanSummary.activeLoans.forEach((ls: any) => {
+    portfolioInfo[ls.orderBook] ||= {activeLoans: 0, totalLoaned: 0, totalEarnings: 0, floor: 0, value: 0}
+    portfolioInfo[ls.orderBook].activeLoans += 1;
+    portfolioInfo[ls.orderBook].totalLoaned += ls.amountSol;
+    portfolioInfo[ls.orderBook].totalEarnings += ls.earnings;
+  })
 
   return (
-    <div className='h-screen flex flex-row border divide-x'>
-      <section className='w-1/3'>
+    <div className='flex flex-row w-full border divide-x dark:text-gray-200'>
+      <section className='w-1/3 flex-grow'>
         <header>
-          <h2 className="mb-3 text-4xl font-bold dark:text-gray-200">Portfolio</h2>
+          <h2 className="my-3 text-4xl font-bold text-center">Portfolio</h2>
         </header>
+        <div className="flex-1 overflow-auto">
+          <table className="w-full">
+            <thead className="border-b">
+              <tr className='text-left'>
+                <th className="p-2">Collection</th>
+                <th className="p-2 text-right">Loans</th>
+                <th className="p-2 text-right">Total</th>
+                <th className="p-2 text-right">Earnings</th>
+                <th className="p-2 text-right">Floor</th>
+                <th className="p-2 text-right">Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="hover:bg-gray-100 cursor-pointer dark:hover:text-black text-sm">
+                <td className="p-2">All Collections</td>
+              </tr>
+              {collections.map((collection: any) => {
+                return (
+                  <tr className="hover:bg-gray-100 cursor-pointer dark:hover:text-black text-sm">
+                    <td className="p-2">{collection.name}</td>
+                    <td className="p-2 text-right">{portfolioInfo[collection.id].activeLoans}</td>
+                    <td className="p-2 text-right">{Number(portfolioInfo[collection.id].totalLoaned).toFixed(2)}</td>
+                    <td className="p-2 text-right">{Number(portfolioInfo[collection.id].totalEarnings).toFixed(2)}</td>
+                    <td className="p-2 text-right">{portfolioInfo[collection.id].floor}</td>
+                    <td className="p-2 text-right">{portfolioInfo[collection.id].value}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </section>
       <section className='w-2/3'>
         <header>
-          <h2 className="mb-3 text-4xl font-bold dark:text-gray-200">
+          <h2 className="my-3 text-4xl font-bold text-center">
             Collection
           </h2>
         </header>
